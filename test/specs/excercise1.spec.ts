cf888926a724ae924555198baf32c51130cae9f9
yenh-cs <62593685+yenh-cs@users.noreply.github.com>
2022-04-12 09:27:48 +0200

    Update excercise1.spec.ts

--- test/specs/excercise1.spec.ts
+++ test/specs/excercise1.spec.ts
@@ -14,10 +14,4 @@ describe('Google Search', () => {
     const textUnderShadowRoot = await shadowRootElement.shadow$('span').getText();
     expect(textUnderShadowRoot).toEqual("This is a demo website for testing purpose");
   });
-  
-  // Use the website http://main.ctqatest.info/test.php  and perform the following tasks.
-  // Go to the login page and enter the username as test@test.com and password as ThisIs@T3st. Check if there is an error displayed.
-  it('should be able to login', async() => {
-   
-  });
 });
